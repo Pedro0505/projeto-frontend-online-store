dd45419e3e9642243c35e1d4a34dd969a1a63dd9
lucas-morais <lucasmoraisss@gmail.com>
2021-10-25 18:21:56 -0300

    requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import ListCategories from '../components/ListCategories';
+import * as api from '../services/api';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const response = await api.getCategories();
+    this.setState({
+      categories: response,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <ListCategories categories={ categories } />
       </div>
     );
   }
