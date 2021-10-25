bd48266f7c72ff23d9761ab495703d9fd6f3498c
Carlos Barros <carlos_dbs@hotmail.com>
2021-10-25 17:43:09 -0300

    Buscando produtos na api por pesquisa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 
+import * as api from '../services/api';
+
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      products: [],
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ search: value });
+  }
+
+  handleSearch = async () => {
+    const { search } = this.state;
+    const response = await api.getProductsFromCategoryAndQuery(search);
+    const products = response.results;
+    this.setState({ products });
+  }
+
   render() {
+    const { search } = this.state;
     return (
       <div>
-        <input data-testid="query-input" type="text" />
+        <input
+          data-testid="query-input"
+          type="text"
+          value={ search }
+          onChange={ this.handleChange }
+        />
         <button
           type="submit"
           data-testid="query-button"
+          onClick={ this.handleSearch }
         >
           Buscar
         </button>
