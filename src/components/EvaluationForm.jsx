012e0c20a4c43445703aa218176389f2b62ad546
lucas-morais <lucasmoraisss@gmail.com>
2021-10-26 22:46:50 -0300

    Requisito 11 - sem estilizaçao

--- src/components/EvaluationForm.jsx
+++ src/components/EvaluationForm.jsx
@@ -1,55 +1,108 @@
-import React, {Component} from 'react';
-import Rating from './Rating';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class EvaluationForm extends Component {
   constructor() {
     super();
 
     this.state = {
-      email: '',
+      name: '',
       evaluationText: '',
       rating: 0,
-    }
+    };
   }
 
   handleChange = ({ target }) => {
     const { name, value } = target;
-    console.log(name)
     this.setState({
-      [name]: value
-    });
-  }  
-
-  hanldeRating = (stars) => {
-    this.setState({
-      rating: stars
+      [name]: value,
     });
   }
 
-  render() { 
+  saveEvaluation = () => {
+    const { updateEvaluations } = this.props;
+    const { name, evaluationText, rating } = this.state;
+    const newEvaluation = { name, rating, evaluationText };
+    updateEvaluations(newEvaluation);
+  }
+
+  render() {
     const { email, evaluation } = this.state;
     return (
       <form>
-        <input 
-          type="email" 
-          name="email" 
-          placeholder="email"
-          onChange={this.handleChange}
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          onChange={ this.handleChange }
           value={ email }
         />
-        <Rating handleRating={ this.hanldeRating }/>
-        <textarea 
-          name="evaluationText" 
-          cols="30" 
-          rows="10" 
+        <label htmlFor="1">
+          1
+          <input
+            type="radio"
+            value="1"
+            name="rating"
+            id="1"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="2">
+          2
+          <input
+            type="radio"
+            value="2"
+            name="rating"
+            id="2"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="3">
+          3
+          <input
+            type="radio"
+            value="3"
+            name="rating"
+            id="3"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="4">
+          4
+          <input
+            type="radio"
+            value="4"
+            name="rating"
+            id="4"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="5">
+          5
+          <input
+            type="radio"
+            value="5"
+            name="rating"
+            id="5"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <textarea
+          name="evaluationText"
+          cols="30"
+          rows="10"
           data-testid="product-detail-evaluation"
           onChange={ this.handleChange }
-          value={evaluation} 
-        ></textarea>
-        <button type="button" name="e">Avaliar</button>
+          value={ evaluation }
+        />
+        <button type="button" name="e" onClick={ this.saveEvaluation }>Avaliar</button>
       </form>
     );
   }
 }
- 
+
+EvaluationForm.propTypes = {
+  updateEvaluations: PropTypes.func.isRequired,
+};
+
 export default EvaluationForm;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class Rating extends Component {
-  
   // getRating = ({ target }) => {
   //   const { handleRating }= this.props;
   //   const rate  = parseInt(target.id) + 1;
@@ -9,26 +8,26 @@ class Rating extends Component {
   //   console.log(target.id)
   //   handleRating(rate);
   // }
-  
+
   // handleRatingHover = ( { target }) => {
   //   {}
   // }
 
   // createStar = (id) => {
   //   return (
-  //     <span key={id}> 
-        
+  //     <span key={id}>
+
   //       <AiTwotoneStar id={ id } color="white" onClick={ this.getRating } onMouseOver={ this.handleRatingHover }/>
   //     </span>
   //   );
   // }
 
-  render() { 
+  render() {
     return (
-      <div></div>
+      <div />
       //  new Array(5).fill(0).map((_arr, index) => this.createStar(index))
     );
   }
 }
- 
+
 export default Rating;
