1e4ca7ddad72bfe5af60dc1f6ac06b22d397fcf2
Pedro Henrique <pedrohrds05@hotmail.com>
2021-10-27 14:22:54 -0300

    Inicio do req 12

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import './App.css';
 import ProductDetail from './pages/ProductDetail';
 import CartPage from './pages/CartPage';
+import FormPay from './components/FormPay';
 
 class App extends Component {
   render() {
@@ -13,6 +13,8 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={ Home } />
             <Route path="/CartPage" exact component={ CartPage } />
+            <Route path="/CartPage" exact component={ CartPage } />
+            <Route path="/checkout" exact component={ FormPay } />
             <Route path="/product-detail/:id" exact component={ ProductDetail } />
           </Switch>
         </BrowserRouter>
