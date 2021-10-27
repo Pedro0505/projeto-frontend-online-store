3a1b179380cc5279477ce107b078f0f46b8ca6ce
Carlos Barros <carlos_dbs@hotmail.com>
2021-10-27 11:51:07 -0300

    Refatorando CartItem para receber o novo atributo quantity

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,19 +1,35 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class CartItem extends Component {
   render() {
-    const { product } = this.props;
+    const { item } = this.props;
     return (
       <div>
         <h2
           data-testid="shopping-cart-product-name"
         >
-          Item:
+          {item.title}
         </h2>
-        <p data-testid="shopping-cart-product-quantity">Quantidade:</p>
+        <p
+          data-testid="shopping-cart-product-quantity"
+        >
+          {`Quantidade: ${item.quantity}`}
+        </p>
       </div>
     );
   }
 }
+
+CartItem.defaultProps = {
+  item: {},
+};
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    quantity: PropTypes.number,
+    title: PropTypes.string,
+  }),
+};
 
 export default CartItem;
